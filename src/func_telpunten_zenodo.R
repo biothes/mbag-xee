beac574b94feed4f4b4a3e287420a8589d6acd50
Kato <103944685+biothes@users.noreply.github.com>
2024-06-26 16:57:30 +0200

    update meeting met Ward

--- src/func_telpunten_zenodo.R
+++ src/func_telpunten_zenodo.R
@@ -2,7 +2,7 @@
 # print(.libPaths())
 # print(installed.packages())
 
-library(inborutils)
+library(inborutils) #zen4r gebruiken
 inborutils::download_zenodo(
   doi = '10.5281/zenodo.10890741', #random file als placeholder
   path = 'C:/Users/kato_vanpoucke/Documents/git/mbag-xee/data/01-telcirkels') 
