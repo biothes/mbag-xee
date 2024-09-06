a5e7d3fc568df3de01219c1a8118ac15a2912d66
Kato <103944685+biothes@users.noreply.github.com>
2024-09-06 11:17:23 +0200

    start update rond input nieuwe kaartlagen

--- src/func_telpunten_zenodo.R
+++ src/func_telpunten_zenodo.R
@@ -3,8 +3,19 @@
 # print(installed.packages())
 
 library(inborutils) #zen4r gebruiken
+
+
+#Check if argument is passed, otherwise use a default path
+#trailingOnly:only arguments after --args are returned
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  stop("No path provided to store the file.")
+} else {
+  download_path <- args[1]
+}
+
 inborutils::download_zenodo(
   doi = '10.5281/zenodo.10890741', #random file als placeholder
-  path = 'C:/Users/kato_vanpoucke/Documents/git/mbag-xee/data/01-telcirkels') 
+  path = download_path) 
 
 print('File was downloaded')
